db40af8d8ff3f9cdc394740d37d58932bf697576
PoorMilk <86163938+PoorMilk@users.noreply.github.com>
2026-04-18 10:58:30 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -7,21 +7,3 @@
 
 # 修改主机名
 sed -i 's/OpenWrt/NW874-360T7/g' package/base-files/files/bin/config_generate
-
-# Coremark 开机自启
-cat > package/base-files/files/etc/init.d/coremark << 'EOF'
-#!/bin/sh /etc/rc.common
-START=99
-start() {
-    [ ! -f /etc/bench.log ] && /etc/coremark.sh &
-}
-EOF
-chmod +x package/base-files/files/etc/init.d/coremark
-ln -sf ../init.d/coremark package/base-files/files/etc/rc.d/S99coremark
-
-cat > package/base-files/files/etc/coremark.sh << 'EOF'
-#!/bin/sh
-sleep 2
-coremark > /etc/bench.log
-EOF
-chmod +x package/base-files/files/etc/coremark.sh
